5dfe73d8a1b1fea838f0868c2918da2e100760ac
unknown <pebuka351@gmail.com>
2024-02-13 12:22:31 +0100

    Game Logic

--- components/Title.js
+++ components/Title.js
@@ -1,11 +1,23 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Title({ children }) {
+export default function Title({ children, boxStyle, textStyling }) {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={[styles.container, boxStyle]}>
+      <Text style={[styles.textStyle, textStyling]}>{children}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 2,
+    borderColor: "white",
+    paddingHorizontal: 40,
+    paddingVertical: 13,
+  },
+  textStyle: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Alert } from "react-native";
+import Title from "../components/Title";
+import Button from "../components/Button";
+
+let minBoundry = 1;
+let maxBoundry = 100;
 
 const generateRandomNumber = (min, max, exclude) => {
   let number = Math.floor(Math.random() * (max - min)) + 1;
@@ -11,11 +16,56 @@ const generateRandomNumber = (min, max, exclude) => {
 };
 
 export default function GameScreen({ enteredNumber }) {
-  const initialGuess = generateRandomNumber(1, 100, enteredNumber);
+  const initialGuess = generateRandomNumber(
+    minBoundry,
+    maxBoundry,
+    enteredNumber
+  );
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < enteredNumber) ||
+      (direction !== "lower" && currentGuess > enteredNumber)
+    ) {
+      Alert.alert("Don't Lie!", "You know that you're wrong.", [
+        { text: "Sorry", style: "cancel" },
+      ]);
+
+      return;
+    }
+    if (direction === "lower") {
+      maxBoundry = currentGuess - 1;
+    } else {
+      minBoundry = currentGuess + 1;
+    }
+    let newRandomNumber = generateRandomNumber(
+      minBoundry,
+      maxBoundry,
+      currentGuess
+    );
+    setCurrentGuess(newRandomNumber);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Game Screen {enteredNumber}</Text>
+      <Title boxStyle={{ marginBottom: 25 }}>Oponent's Guess</Title>
+      <Title boxStyle={styles.numberBox} textStyling={{ fontSize: 32 }}>
+        {currentGuess}
+      </Title>
+      <Text style={{ marginBottom: 10 }}>Higher or Lower?</Text>
+      <View style={styles.buttonBox}>
+        <Button
+          onClick={() => nextGuessHandler("lower")}
+          style={styles.buttonStyle}
+          title="-"
+        />
+        <Button
+          onClick={() => nextGuessHandler("greater")}
+          style={styles.buttonStyle}
+          title="+"
+        />
+      </View>
     </View>
   );
 }
@@ -25,5 +75,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  numberBox: {
+    marginBottom: 25,
+    paddingVertical: 22,
+    paddingHorizontal: 55,
+    borderRadius: 6,
+  },
+  buttonStyle: {
+    backgroundColor: "white",
+    flex: 1,
+    paddingVertical: 15,
+  },
+  buttonBox: {
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    gap: 10,
   },
 });
